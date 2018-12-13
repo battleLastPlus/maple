667a02ca29075c2d10ef9b1357ea0126ec0cab52
Core <Core@DESKTOP-KIIDKBU>
2018-12-13 11:51:03 +0800

    ignore

--- maple-backstage-web/src/main/java/com/maple/controller/RunApplication.java
+++ maple-backstage-web/src/main/java/com/maple/controller/RunApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RunApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunApplication.class, args);
+		SpringApplication sa=new SpringApplication(RunApplication.class);
+		// 禁用devTools热部署
+		//System.setProperty("spring.devtools.restart.enabled", "false");
+		// 禁用命令行更改application.properties属性
+		sa.setAddCommandLineProperties(false);
+		sa.run(args);
 	}
 }
