ce9c9b1a559850ec41473395c9145b734fdbe27e
yrfayrf <1004388972@qq.com>
2018-12-25 17:57:39 +0800

    create pojo

---
+++ maple-dao/src/main/java/com/maple/dao/user/RolePermissionMapper.java
@@ -0,0 +1,11 @@
+package com.maple.dao.user;
+
+import com.maple.pojo.user.RolePermissionKey;
+
+public interface RolePermissionMapper {
+    int deleteByPrimaryKey(RolePermissionKey key);
+
+    int insert(RolePermissionKey record);
+
+    int insertSelective(RolePermissionKey record);
+}
---
+++ maple-dao/src/main/java/com/maple/dao/user/UserRoleMapper.java
@@ -0,0 +1,11 @@
+package com.maple.dao.user;
+
+import com.maple.pojo.user.UserRoleKey;
+
+public interface UserRoleMapper {
+    int deleteByPrimaryKey(UserRoleKey key);
+
+    int insert(UserRoleKey record);
+
+    int insertSelective(UserRoleKey record);
+}
